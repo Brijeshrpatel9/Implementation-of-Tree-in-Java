5f167855da26599f26dc3642ff8ea8c6e8782aab
Brijesh Patel <Brijesh.Patel@finra.org>
2015-10-07 14:40:48 -0400

    Remove Space

--- BTree.java
+++ BTree.java
@@ -38,7 +38,8 @@ public class BTree<Key extends Comparable<Key>, Value>  {
 
     // search for given key, return associated value; return null if no such key
     public Value get(Key key) { return search(root, key, HT); }
-    private Value search(Node x, Key key, int ht) {
+    
+	private Value search(Node x, Key key, int ht) {
         Entry[] children = x.children;
 
         // external node
